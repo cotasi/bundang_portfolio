36ee59ed2654e85bc365d70a64c5f14ca274888f
cotasi <cotasi135@gmail.com>
2023-10-25 16:49:02 +0900

    개인작업

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const over = document.querySelectorAll("#gnb>li");
+const height = document.querySelectorAll(".smenu");
+
+over.addEventListener("mouseover",function(){
+    height.style.display = "block";
+});
