e0a4acbecc749b94b5da58d86cc808c6b1393fc4
cotasi <cotasi135@gmail.com>
2023-10-30 11:45:01 +0900

    웹기능사연습3

---
+++ js/web.js
@@ -0,0 +1,5 @@
+$('#gnb').click(function(){});
+$('#gnb>li:nth-child(1)').eq(0).click(function(){});
+$('a>img').click(()=>{});
+$('button[id="cart"]').click(()=>{});
+$("#gnb li").eq(0).find('a').click(()=>{});
